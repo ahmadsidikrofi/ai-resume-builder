9864489afc33c683096570314212a5017d8ae241
ahmadsidikrofi <rofidragon71@gmail.com>
2025-08-09 19:59:39 +0700

    feat: color picker to change color on resume

--- app/(main)/editor/forms/ColorPicker.js
+++ app/(main)/editor/forms/ColorPicker.js
@@ -1,18 +1,29 @@
 import { Button } from "@/components/ui/button";
-import { Popover } from "@/components/ui/popover";
+import { Popover, PopoverContent } from "@/components/ui/popover";
 import { PopoverTrigger } from "@radix-ui/react-popover";
 import { PaletteIcon } from "lucide-react";
 import { useState } from "react";
+import { TwitterPicker } from "react-color";
 
 const ColorPicker = ({ color, onChange }) => {
     const [showPopover, setShowPopover] = useState(false)
     return (
         <Popover open={showPopover} onOpenChange={setShowPopover}>      
             <PopoverTrigger asChild>
-                <Button>
+                <Button
+                    size="icon"
+                    variant="outline"
+                    title="Change resume color"
+                    onClick={() => setShowPopover(true)}
+                >
                     <PaletteIcon className="size-5"/>
                 </Button>
             </PopoverTrigger>
+            <PopoverContent className="border-none bg-transparent shadow-none" align="end">
+                <TwitterPicker 
+                    color={color} onChange={onChange} triangle="top-right"
+                />
+            </PopoverContent>
         </Popover>
     );
 }
--- components/ResumePreview.jsx
+++ components/ResumePreview.jsx
@@ -10,8 +10,8 @@ const ResumePreview = ({ resumeData, className }) => {
     const { width } = useDimensions(containerRef)
 
     const PersonalInfoHeader = ({ resumeData }) => {
-        const { photo, firstName, lastName, jobTitle, city, country, phone, email, colorHext, borderStyle } = resumeData
-        const [ photoSrc, setPhotoSrc ] = useState(photo instanceof File ? "" : photo)
+        const { photo, firstName, lastName, jobTitle, city, country, phone, email, colorHex, borderStyle } = resumeData
+        const [photoSrc, setPhotoSrc] = useState(photo instanceof File ? "" : photo)
 
         useEffect(() => {
             const objectUrl = photo instanceof File ? URL.createObjectURL(photo) : ""
@@ -23,7 +23,7 @@ const ResumePreview = ({ resumeData, className }) => {
         return (
             <div className="flex items-center gap-6">
                 {photoSrc && (
-                    <Image 
+                    <Image
                         src={photoSrc}
                         width={100}
                         height={100}
@@ -33,8 +33,16 @@ const ResumePreview = ({ resumeData, className }) => {
                 )}
                 <div className="space-y-2.5">
                     <div className="space-y-1">
-                        <p className="text-3xl font-bold">{firstName} {lastName}</p>
-                        <p className="font-medium">{jobTitle}</p>
+                        <p className="text-3xl font-bold"
+                            style={{
+                                color: colorHex
+                            }}
+                        >{firstName} {lastName}</p>
+                        <p className="font-medium"
+                            style={{
+                                color: colorHex
+                            }}
+                        >{jobTitle}</p>
                         <p className="text-xs text-gray-500">
                             {city}
                             {city && country ? ", " : ""}
@@ -49,121 +57,157 @@ const ResumePreview = ({ resumeData, className }) => {
     }
 
     const SummarySection = ({ resumeData }) => {
-        const { summary } = resumeData
+        const { summary, colorHex } = resumeData
         if (!summary) return null
 
         return (
             <>
-                <hr className="border-2"/>
+                <hr className="border-2" 
+                    style={{ 
+                        borderColor: colorHex
+                    }}                
+                />
                 <div className="break-inside-avoid space-y-3">
-                    <p className="text-lg font-semibold">Professional profile</p>
-                    <div className="whitespace-pre-line text-sm">{ summary }</div>
+                    <p className="text-lg font-semibold"
+                        style={{
+                            color: colorHex
+                        }}
+                    >Professional profile</p>
+                    <div className="whitespace-pre-line text-sm">{summary}</div>
                 </div>
             </>
         )
     }
 
     const WorkExperienceSection = ({ resumeData }) => {
-        const { workExperiences } = resumeData
-        
+        const { workExperiences, colorHex } = resumeData
+
         const workExperiencesNotEmpty = workExperiences?.filter((exp) => Object.values(exp).filter(Boolean).length > 0)
         if (!workExperiencesNotEmpty?.length) return null
 
         return (
-          <>
-            <hr className="border-2" />
-            <div className="space-y-3">
-              <p className="text-lg font-semibold">Woek experience</p>
-              {workExperiencesNotEmpty.map((exp, i) => (
-                <div key={i} className="break-inside-avoid space-y-1">
-                    <div className="flex items-center justify-between text-sm font-semibold">
-                        <span>{exp.position}</span>
-                        {exp.startDate && (
-                            <span>
-                                {formatDate(exp.startDate, "MM/yyyy")} - {" "}
-                                {exp.endDate ? formatDate(exp.endDate, "MM/yyyy") : "Present"}
-                            </span>
-                        )}
-                    </div>
-                    <p className="text-xs font-semibold">{exp.company}</p>
-                    <div className="whitespace-pre-line text-xs ">{exp.description}</div>
+            <>
+                <hr className="border-2" 
+                    style={{ 
+                        borderColor: colorHex
+                    }}
+                />
+                <div className="space-y-3">
+                    <p className="text-lg font-semibold"
+                        style={{ 
+                            color: colorHex
+                        }}
+                    >Woek experience</p>
+                    {workExperiencesNotEmpty.map((exp, i) => (
+                        <div key={i} className="break-inside-avoid space-y-1">
+                            <div className="flex items-center justify-between text-sm font-semibold">
+                                <span>{exp.position}</span>
+                                {exp.startDate && (
+                                    <span>
+                                        {formatDate(exp.startDate, "MM/yyyy")} - {" "}
+                                        {exp.endDate ? formatDate(exp.endDate, "MM/yyyy") : "Present"}
+                                    </span>
+                                )}
+                            </div>
+                            <p className="text-xs font-semibold">{exp.company}</p>
+                            <div className="whitespace-pre-line text-xs ">{exp.description}</div>
+                        </div>
+                    ))}
                 </div>
-              ))}
-            </div>
-          </>
+            </>
         );
     }
 
     const EducationSection = ({ resumeData }) => {
-        const { educations } = resumeData
+        const { educations, colorHex } = resumeData
         const educationsNotEmpty = educations?.filter((edu) => Object.values(edu).filter(Boolean).length > 0)
         if (!educationsNotEmpty?.length) return null
 
         return (
-          <>
-            <hr className="border-2" />
-            <div className="space-y-3">
-              <p className="text-lg font-semibold">Education</p>
-              {educationsNotEmpty.map((edu, i) => (
-                <div key={i} className="break-inside-avoid space-y-1">
-                  <div className="flex items-center justify-between text-sm font-semibold">
-                    <span>{edu.degree}</span>
-                    {edu.startDate && (
-                      <span>
-                        {formatDate(edu.startDate, "MM/yyyy")} -{" "}
-                        {edu.endDate
-                          ? formatDate(edu.endDate, "MM/yyyy")
-                          : "Present"}
-                      </span>
-                    )}
-                  </div>
-                  <p className="text-xs font-semibold">{edu.school}</p>
+            <>
+                <hr className="border-2" 
+                    style={{ 
+                        borderColor: colorHex
+                    }}                
+                />
+                <div className="space-y-3">
+                    <p className="text-lg font-semibold"
+                        style={{ 
+                            color: colorHex
+                        }}
+                    >Education</p>
+                    {educationsNotEmpty.map((edu, i) => (
+                        <div key={i} className="break-inside-avoid space-y-1">
+                            <div className="flex items-center justify-between text-sm font-semibold">
+                                <span>{edu.degree}</span>
+                                {edu.startDate && (
+                                    <span>
+                                        {formatDate(edu.startDate, "MM/yyyy")} -{" "}
+                                        {edu.endDate
+                                            ? formatDate(edu.endDate, "MM/yyyy")
+                                            : "Present"}
+                                    </span>
+                                )}
+                            </div>
+                            <p className="text-xs font-semibold">{edu.school}</p>
+                        </div>
+                    ))}
                 </div>
-              ))}
-            </div>
-          </>
+            </>
         );
     }
 
     const SkillsSection = ({ resumeData }) => {
-        const { skills } = resumeData
+        const { skills, colorHex } = resumeData
         if (!skills?.length) return null
 
         return (
-          <>
-            <hr className="border-2" />
-            <div className="break-inside-avoid space-y-3">
-              <p className="text-lg font-semibold">Skills</p>
-              <div className="break-inside-avoid flex flex-wrap gap-2">
-                {skills.map((skill, i) => (
-                  <Badge className="bg-black text-white" key={i}>
-                    {skill}
-                  </Badge>
-                ))}
-              </div>
-            </div>
-          </>
+            <>
+                <hr className="border-2" 
+                    style={{ 
+                        borderColor: colorHex
+                    }}
+                />
+                <div className="break-inside-avoid space-y-3">
+                    <p className="text-lg font-semibold"
+                        style={{ 
+                            color: colorHex
+                        }}
+                    >Skills</p>
+                    <div className="break-inside-avoid flex flex-wrap gap-2">
+                        {skills.map((skill, i) => (
+                            <Badge className="bg-black text-white" key={i}
+                                style={{ 
+                                    backgroundColor: colorHex
+                                }}
+                            >
+                                {skill}
+                            </Badge>
+                        ))}
+                    </div>
+                </div>
+            </>
         );
-    } 
+    }
 
-    return ( 
+    return (
         <div className={cn("h-fit w-full aspect-[210/297] bg-white text-black", className)}
             ref={containerRef}
         >
             <div
-                className={cn("space-y-6 p-6", !width && "invisible")} 
-                style={{ 
+                className={cn("space-y-6 p-6", !width && "invisible")}
+                style={{
                     zoom: (1 / 794) * width
                 }}
             >
-                <PersonalInfoHeader resumeData={resumeData}/>
-                <SummarySection resumeData={resumeData}/>
-                <WorkExperienceSection resumeData={resumeData}/>
-                <EducationSection resumeData={resumeData}/>
-                <SkillsSection resumeData={resumeData}/>
+                <PersonalInfoHeader resumeData={resumeData} />
+                <SummarySection resumeData={resumeData} />
+                <WorkExperienceSection resumeData={resumeData} />
+                <EducationSection resumeData={resumeData} />
+                <SkillsSection resumeData={resumeData} />
             </div>
         </div>
     )
 }
- 
+
 export default ResumePreview;
